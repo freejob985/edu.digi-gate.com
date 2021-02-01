b8ea9b2b8e32e50442378d543f16a956ad1edd0e
freejob985 <freejob985@gmail.com>
2021-02-01 21:23:03 +0200

    شسي

--- resources/views/web/default/view/parts/newest.blade.php
+++ resources/views/web/default/view/parts/newest.blade.php
@@ -15,20 +15,21 @@
                                 <img src="{{ !empty($meta['thumbnail']) ? $meta['thumbnail'] : '' }}"/>
                                 <h3>{!! truncate($new->title,30) !!}</h3>
                                 <div class="footer">
-                                    <span class="avatar" title="{{ !empty($content['user']['name']) ? $content['user']['name'] : '' }}" onclick="window.location.href = '/profile/{{ $content['user']['id'] }}'"><img src="{{ get_user_meta($content['user_id'],'avatar',get_option('default_user_avatar','')) }}"></span>
-
-                                        <label class="pull-right content-clock">{{ contentDuration($content['id']) }}</label>
-                                        <span class="boxicon mdi mdi-clock pull-right"></span>
-
+                                    @if(isset($new->user))
+                                    <span class="avatar" title="{{ $new->user->name }}" onclick="window.location.href = '/profile/{{ $new->user->id }}'">
+                                        <img src="{{ get_user_meta($new->user_id,'avatar',get_option('default_user_avatar','')) }}"/>
+                                    </span>
+                                    @endif
+                                    <label class="pull-right content-clock">{!! contentDuration($new->id) !!}</label>
+                                    <span class="boxicon mdi mdi-clock pull-right"></span>
                                     <span class="boxicon mdi mdi-wallet pull-left"></span>
-                                    {{--  @dd("Catch errors for script and full tracking ( 2)");  --}}
-                                    @php
-                                   $contents_meta = DB::table('contents_meta')->where('option','Discount')->where('content_id',$content['id'])->value('value');
-                                   $content_rate[] = DB::table('content_rate')->where('content_id',$content['id'])->value('rate');
-                                    @endphp
-                                     <label class="pull-left" style="padding-left: 4%;text-decoration: line-through; "> @if(isset($content['metas']['price'])) {{ price($content['id'],$content['category_id'],$content['metas']['price'])['price_txt'] ?? 0 }} @endif</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <label class="pull-left"> {{$contents_meta}}$</label>
-                                   
+                                    <label class="pull-left">
+                                        @if(isset($meta['price']) && $meta['price']>0)
+                                            {{currencySign()}}{{ price($new->id,$new->category_id,$meta['price'])['price'] }}
+                                        @else
+                                            {{ trans('main.free') }}
+                                        @endif
+                                    </label>
                                 </div>
                             </a>
                         </div>
